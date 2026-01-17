b51a289f45c31e0dfdbd72d9931bcd6ab354b719
RajeshLaffey <63221237+RajeshLaffey@users.noreply.github.com>
2026-01-16 17:24:36 -0800

    Update solution.py

--- solutions/solution-day001-array-swap/solution.py
+++ solutions/solution-day001-array-swap/solution.py
@@ -1,1 +1,16 @@
+'''
+DAY 01 - ARRAY SWAP
+Given an array with two values, return an array with the values swapped.
+'''
+
+def array_swap(arr):
+    a, b = arr
+    arr = [b, a]
+    
+    return arr
+
+print(array_swap(["A", "B"]))     # strings
+print(array_swap([1, 2]))         # integers
+print(array_swap(["A", 10]))      # mixed types
+print(array_swap([True, None]))
 
