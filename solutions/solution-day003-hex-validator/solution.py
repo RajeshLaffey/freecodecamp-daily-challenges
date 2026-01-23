fc2bc981f7124296fd25721269423dafa1754394
RajeshLaffey <63221237+RajeshLaffey@users.noreply.github.com>
2026-01-23 08:17:26 -0800

    Update solution.py
    add code.

--- solutions/solution-day003-hex-validator/solution.py
+++ solutions/solution-day003-hex-validator/solution.py
@@ -1,1 +1,15 @@
+def is_valid_hex(s):
+    hex_char = "abcedf0123456789"
+    modified_s = s[1:]
 
+    if not isinstance(s, str):
+        return False
+    
+    if len(modified_s) not in (3,6):
+        return False
+    
+    if s.startswith("#"):
+        for letter in modified_s:
+            if letter.lower() not in hex_char:
+                return False
+        return True
